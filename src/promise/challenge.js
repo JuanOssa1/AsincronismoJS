0108e2fa0f7595b6d20ac8bdce147aa66206b3a5
JuanOssa1 <juan.ossa@hotmail.com>
2022-02-22 17:02:16 -0500

    Desafio completado con promesas1

--- src/promise/challenge.js
+++ src/promise/challenge.js
@@ -1,5 +1,5 @@
-const fetchData = requeired('../utils/fetchData.js');
-let API = 'https://rickandmortyapi.com/api/character/';
+const fetchData = require('../utils/fetchData.js');
+let API = 'https://rickandmortyapi.com/api/character/';/**Poner el slach al final */
 /**Aqui estoy haciendo como con los callbacks le estoy diciendo que haga el paso a paso para poder
  * llegar a lo que quiero buscar del personaje y nuveamente funciona como una especie de pipe
  */
@@ -13,6 +13,6 @@ fetchData(API)
         return fetchData(data.origin.url)
     })
     .then(data => {
-        console.log(data.dimension)
+        console.log(data.dimension) 
     })
     .catch(err => console.error(err));
